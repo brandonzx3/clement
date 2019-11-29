b5accae6bfe840f3cc623f843ed057e1504924a0
brandon <btherr2003@gmail.com>
2019-11-29 15:31:38 -0600

    added free assets to replace later

---
+++ Clement/Assets/Scripts/Chest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour {
+
+    public GameObject player;
+
+    public Sprite open;
+    public Sprite closed;
+
+    public SpriteRenderer r;
+
+    public bool opened = false;
+
+	// Update is called once per frame
+	void Update () {
+        float distance = Vector2.Distance(transform.position, player.transform.position);
+
+        if(distance <= 1)
+        {
+            if(Input.GetKeyDown(KeyCode.Return))
+            {
+                opened = true;
+            }
+        }
+
+        if(opened)
+        {
+            r.sprite = open;
+        }
+        else
+        {
+            r.sprite = closed;
+        }
+    }
+}
